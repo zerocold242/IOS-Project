5f7b42ace7e7971ce0890012cf597e2857722327
zerocold242 <alexrylexx@gmail.com>
2022-06-09 22:53:26 +0300

    ДЗ - 2.2

--- Navigation-/Controllers/FeedViewController.swift
+++ Navigation-/Controllers/FeedViewController.swift
@@ -9,40 +9,66 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    private lazy var button: UIButton = {
-        
-        let button = UIButton(frame: CGRect(x: self.view.bounds.width / 2 - 100,
-                                            y: self.view.bounds.height / 2 - 25,
-                                            width: 200,
-                                            height: 50))
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 12
-        button.setTitle("Go to the Post", for: .normal)
-        button.setTitleColor(.lightGray, for: .highlighted)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        
-        return button
-    } ()
-    
     var post: Post = .init(title: "My first Post")
     
-    private func setButton() {
-        
-        self.view.addSubview(self.button)
+    let stackView: UIStackView
+    
+    init (){
+        stackView = UIStackView().self
+        super.init(nibName: nil, bundle: nil)
     }
     
-    @objc private func buttonAction() {
-        
-        let postViewController = PostViewController()
-        self.navigationController?.pushViewController(postViewController, animated: true)
-        postViewController.titlePost = post.title
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func showPost(){
+        let postVc = PostViewController()
+        navigationController?.pushViewController(postVc, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemGray4
-        setButton()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        
+        let firstButton = UIButton()
+        firstButton.translatesAutoresizingMaskIntoConstraints = false
+        firstButton.backgroundColor = .systemBlue
+        firstButton.layer.cornerRadius = 10
+        firstButton.layer.shadowColor = UIColor.black.cgColor
+        firstButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        firstButton.layer.shadowOpacity = 1
+        firstButton.layer.shadowRadius = 5
+        firstButton.setTitle("Go to the PostViewController", for: .normal)
+        firstButton.setTitleColor(.lightGray, for: .highlighted)
+        firstButton.addTarget(self, action: #selector(showPost), for: .touchUpInside)
+        stackView.addArrangedSubview(firstButton)
+        
+        let secondButton = UIButton()
+        secondButton.translatesAutoresizingMaskIntoConstraints = false
+        secondButton.backgroundColor = .blue
+        secondButton.layer.cornerRadius = 10
+        secondButton.layer.shadowColor = UIColor.black.cgColor
+        secondButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        secondButton.layer.shadowOpacity = 1
+        secondButton.layer.shadowRadius = 5
+        secondButton.setTitle("Go to the PostViewController", for: .normal)
+        secondButton.setTitleColor(.lightGray, for: .highlighted)
+        secondButton.addTarget(self, action: #selector(showPost), for: .touchUpInside)
+        stackView.addArrangedSubview(secondButton)
+        
+        view.addSubview(stackView)
+        
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -10).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: view.bounds.height / 5).isActive = true
     }
 }
