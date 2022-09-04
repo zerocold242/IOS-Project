cc282c115f4070d0144e6a43abc2bf3827ed357b
zerocold242 <alexrylexx@gmail.com>
2022-09-04 21:11:06 +0300

    TestUserService правки

--- Navigation-/UserData/TestUserService.swift
+++ Navigation-/UserData/TestUserService.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class TestUserService: UserService {
     
-    var testingUser = User(login: "testLogin", fullName: "testFullName", avatar: UIImage(named: "f16"), userStaus: "testMode")
+    var testingUser = User(login: "testLogin", fullName: "Test Mode", avatar: UIImage(named: "f16"), userStaus: "testMode")
     
     func getLogin(login: String) -> User? {
         
