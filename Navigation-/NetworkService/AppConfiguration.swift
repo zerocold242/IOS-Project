6016d11c04a19f9f0f15fc621f8d1e1e238d4e07
zerocold242 <alexrylexx@gmail.com>
2022-10-10 17:39:48 +0300

    NetworkService/AppConfiguration init

---
+++ Navigation-/NetworkService/AppConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  AppConfiguration.swift
+//  Navigation-
+//
+//  Created by Aleksey Lexx on 10.10.2022.
+//
+
+import Foundation
+
+enum AppConfiguration: String {
+    case urlOne = "https://swapi.dev/api/people/8"
+    case urlTwo = "https://swapi.dev/api/starships/3"
+    case urlThree = "https://swapi.dev/api/planets/5"
+}
---
+++ Navigation-/NetworkService/NetworkService.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkService.swift
+//  Navigation-
+//
+//  Created by Aleksey Lexx on 10.10.2022.
+//
+
+import Foundation
+
+struct NetworkService {
+    
+    static func request() {
+        
+    }
+}
